12b7c317b3936d39f8857c7345b5d906e1617d3e
Trevor <trevor@quanta-inc.com>
2024-02-08 00:24:40 -0500

    feat: responsive category charts

--- src/lib/components/CategoryChart.svelte
+++ src/lib/components/CategoryChart.svelte
@@ -5,14 +5,40 @@
 
 	export let amounts: number[] = [];
 	export let categories: string[] = [];
+
 	let chart: HTMLElement;
+	let smolChart: HTMLElement;
+
 	let apexChart: ApexCharts;
+	let smolApexChart: ApexCharts;
 
 	let options: ApexOptions = {
 		chart: {
 			type: 'pie',
-			height: 400,
-			foreColor: '#fff'
+			height: 350,
+			foreColor: '#fff',
+		},
+		tooltip: {
+			enabled: true,
+			theme: 'dark',
+			y: {
+				formatter: (val) => {
+					return formatCurrency(val);
+				}
+			}
+		},
+		legend: {
+			show: true,
+		},
+		series: amounts.map((d) => parseInt(d.toString())),
+		labels: categories
+	};
+
+	let smolOptions: ApexOptions = {
+		chart: {
+			type: 'pie',
+			height: 1000,
+			foreColor: '#fff',
 		},
 		tooltip: {
 			enabled: true,
@@ -28,19 +54,24 @@
 			position: 'bottom'
 		},
 		series: amounts.map((d) => parseInt(d.toString())),
-		labels: categories.map((category) => category)
+		labels: categories
 	};
+	
 	onMount(async () => {
 		const ApexCharts = (await import('apexcharts')).default;
 		apexChart = new ApexCharts(chart, options);
+		smolApexChart = new ApexCharts(smolChart, smolOptions);
 		await apexChart.render();
+		await smolApexChart.render();
 	});
 
 	onDestroy(() => {
 		apexChart.destroy();
+		smolApexChart.destroy();
 	});
 </script>
 
-<div class="h-96 overflow-y-auto">
-	<div id="chart" bind:this={chart} />
+<div class="h-96 overflow-y-auto overflow-x-hidden">
+	<div id="chart" class="hidden xl:block" bind:this={chart} />
+	<div id="smolchart" class="block xl:hidden" bind:this={smolChart} />
 </div>
